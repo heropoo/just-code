98d64a1ed1aee60149f5e60946953edf6e6c8620
heropoo <aiyouyou1000@163.com>
2018-05-10 12:22:36 +0800

    markdown

--- markdown-preview/builder.php
+++ markdown-preview/builder.php
@@ -28,11 +28,11 @@ if(!file_exists('src/layouts/'.$layout.'.html')){
 $content = file_get_contents($src);
 
 if(!empty($_GET['put_menu'])){  //放入目录列表
-    $dh = opendir('src');
+    $dh = opendir('tmp');
     $menu_src = '';
     while (($file = readdir($dh)) !== false) {
         if(strpos($file, '_menu_') === 0){
-            $menu_src .= file_get_contents('src/'.$file);
+            $menu_src .= file_get_contents('tmp/'.$file);
         }
     }
     closedir($dh);
@@ -52,16 +52,50 @@ if(!empty($_GET['menu'])){  //读取目录
     $dom->loadHTML($html);
     $menu = '';
     $title = $dom->getElementsByTagName('h2')[0]->textContent;
-    $menu .= $title.PHP_EOL;
+    $number = '';
+    if(preg_match("#^([0-9\.]+)\s#", $title, $matches)){
+        $number = $matches[0];
+        $title = str_replace($number, '', $title);
+    }
+
+    $menu .= strlen($number) > 0 ? $number.'['.$title.']('.$input.'.html)'.PHP_EOL : '* ['.$title.']('.$input.'.html)'.PHP_EOL;
 
     foreach($dom->getElementsByTagName('h3') as $h3){
         $menu .= '    * ['.$h3->textContent.']('.$input.'.html#'.urlencode($h3->textContent).')'.PHP_EOL;
         $h3->setAttribute('id', urlencode($h3->textContent));
     }
     $dom->saveHTMLFile($dst);
-    file_put_contents('src/_menu_'.$input.'.md', $menu);
-    echo file_get_contents($dst);
+    file_put_contents('tmp/_menu_'.$number.$input.'.md', $menu);
+    $response =  file_get_contents($dst);
 }else{
     file_put_contents($dst, $html);
-    echo $html;
+    $response =  $html;
 }
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>API生成预览</title>
+    <style>
+        html,body{width: 100%; height: 100%; margin: 0;overflow: hidden}
+        iframe{width: 100%; height: 100%; margin-top: 0; border-style: none;}
+        #top_bar{width: 100%; background-color: #ef6f48; color: #f8f8f8; height:40px; line-height: 40px; padding-left: 10px; border-bottom: 1px solid #ccc;position: fixed;}
+        #iframe_url{color: #f8f8f8; text-decoration:none;}
+        #iframe_url:hover{text-decoration:underline;}
+    </style>
+</head>
+<body>
+<div id="top_bar">文档地址：<a id="iframe_url" title="点击访问" href="dst/<?= $input?>.html">dst/<?= $input?>.html</a></div>
+<iframe src="dst/<?= $input?>.html" onload="loadFrame(this)"></iframe>
+</body>
+<script>
+    function loadFrame(obj){
+        var a = document.getElementById('iframe_url');
+        a.href = a.innerText = obj.contentWindow.location.href;
+    }
+</script>
+</html>
--- markdown-preview/src/_menu_user.md
+++
@@ -1,8 +0,0 @@
-1. 用户账号[user]
-    * [获取注册验证码[userRegGetCode]](user.html#%E8%8E%B7%E5%8F%96%E6%B3%A8%E5%86%8C%E9%AA%8C%E8%AF%81%E7%A0%81%5BuserRegGetCode%5D)
-    * [注册[userRegister]](user.html#%E6%B3%A8%E5%86%8C%5BuserRegister%5D)
-    * [获取登陆验证码 [userLoginGetCode]](user.html#%E8%8E%B7%E5%8F%96%E7%99%BB%E9%99%86%E9%AA%8C%E8%AF%81%E7%A0%81+%5BuserLoginGetCode%5D)
-    * [登陆[userLogin]](user.html#%E7%99%BB%E9%99%86%5BuserLogin%5D)
-    * [快速登陆[userQuickLogin]](user.html#%E5%BF%AB%E9%80%9F%E7%99%BB%E9%99%86%5BuserQuickLogin%5D)
-    * [退出[userLogout]](user.html#%E9%80%80%E5%87%BA%5BuserLogout%5D)
-    * [修改登录密码[userChangePwd]](user.html#%E4%BF%AE%E6%94%B9%E7%99%BB%E5%BD%95%E5%AF%86%E7%A0%81%5BuserChangePwd%5D)
