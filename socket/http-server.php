08a13961eb6f9f1bb008efc07eda90394aa527bc
heropoo <aiyouyou1000@163.com>
2018-07-30 19:27:30 +0800

    socket

--- socket/http-server.php
+++ socket/http-server.php
@@ -12,14 +12,26 @@ echo "Listening $host:$port\r\n";
 
 while (true) {
 	$msgsock = socket_accept($socket);
-	$buff = socket_read($msgsock,9024);
+	$buff = socket_read($msgsock, 9024);
 	echo $buff.PHP_EOL;
 
-	var_dump(strlen(PHP_EOL), strlen("\r\n"), PHP_EOL === "\r\n");
+	$host = '';
+	if(preg_match("/Host:\s(\w+):/", $buff, $matches)){
+		$host = $matches[1];
+	}
 
-	$response = "HTTP/1.1 200 OK\r\n\Content-Type: text/html; charset=utf-8;\r\n\r\n";
+	$headers = [
+		'Content-Type: text/html; charset=utf-8',
+		'Server: nginx/1.12.2',	//冒充nginx
+		//set cookie
+		'Set-Cookie: testcookie=中文; path=/; domain='.$host.'; expires='.gmstrftime("%A, %d-%b-%Y %H:%M:%S GMT",time()+9600), 
+	];
+
+	$response = "HTTP/1.1 200 OK\r\n".implode("\r\n", $headers)."\r\n\r\n";
 	$response .= "hello world\r\n";
-	socket_write($msgsock, $response.$buff);
+
+	echo $response.PHP_EOL;
+	socket_write($msgsock, $response);
 }
 
 socket_close($socket);
