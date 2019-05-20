c466e441696d801a434ce05a2a1bc5ac53990459
heropoo <aiyouyou1000@163.com>
2019-05-20 19:09:13 +0800

    upload large file

---
+++ upload-large-file/upload.php
@@ -0,0 +1,71 @@
+<?php
+
+$file = $_FILES['file'];
+
+$path = __DIR__ . '/uploads';
+
+$name = $_POST['name'];
+$total = $_POST['total'];
+$index = $_POST['index'];
+$size = $_POST['size'];
+
+$dst_file = $path . '/'.$name.'-'.$total.':'.$index;
+
+if ($file["error"] > 0) {
+    echoJson(400, $file["error"]);
+} else {
+    $res = move_uploaded_file($file['tmp_name'], $dst_file);
+    if($res){
+        if(mergeFile($path . '/'.$name, $total, $index, $size)){
+            echoJson(200, 'ok', [
+                'url'=> 'uploads/'.$name
+            ]);
+        }else{
+            echoJson(201, 'shard ok');
+        }
+    }else{
+        echoJson(400, 'shard move_uploaded_file error');
+    }
+}
+
+function mergeFile($name, $total, $index, $size){
+    //$lock_data = @file_get_contents($name.'.lock') or [];
+    //$lock_data['_'.$index] = $size;
+    $already = true;
+    for ($i = 0; $i < $total; $i++){
+        if(!file_exists($name.'-'.$total.':'.$i)){
+            $already = false;
+            break;
+        }
+    }
+
+    //var_dump($name.'.lock', $lock_data);exit;
+    //file_put_contents($name.'.lock', json_encode($lock_data));
+
+    if($already){
+        $file = fopen($name, 'a+');
+        for ($i = 0; $i < $total; $i++){
+            $shardFile = fopen($name.'-'.$total.':'.$i, 'r');
+            $shardData = fread($shardFile, filesize($name.'-'.$total.':'.$i));
+            fwrite($file, $shardData);
+            fclose($shardFile);
+            unlink($name.'-'.$total.':'.$i);
+        }
+        fclose($file);
+
+        return true;
+    }
+    return false;
+}
+
+function echoJson($code, $msg = '', $data = [])
+{
+    header('Content-type: application/json');
+    //sleep(mt_rand(1,10));
+    echo json_encode([
+        'code' => $code,
+        'msg' => $msg,
+        'data' => (object)$data
+    ]);
+    exit();
+}
