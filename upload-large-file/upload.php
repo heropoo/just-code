843d5e0e7e7f37d81646fce8581342edd7177061
heropoo <aiyouyou1000@163.com>
2019-05-22 11:51:17 +0800

    upload large file

--- upload-large-file/upload.php
+++ upload-large-file/upload.php
@@ -1,79 +1,80 @@
 <?php
 
-$file = $_FILES['file'];
-
 $path = __DIR__ . '/uploads';
 
 $name = $_POST['name'];
-$total = $_POST['total'];
-$index = $_POST['index'];
-$size = $_POST['size'];
 $md5 = $_POST['md5'];
 
-$dst_file = $path . '/'.$name.'-'.$total.':'.$index;
+if ($_GET['type'] == 'miao') {
+    if (file_exists($path . '/' . $name) && md5_file($path . '/' . $name) === $md5) {
+        // 文件已存在，妙传
+        echoJson(200, 'ok', [
+            'url' => 'uploads/' . $name,
+        ]);
+    }
+} else if ($_GET['type'] == 'shard') {
+    $file = $_FILES['file'];
 
-if(file_exists($path.'/'.$name) && md5_file($path.'/'.$name) === $md5){
-    // 文件已存在，妙传
-    echoJson(210, 'ok', [
-        'url'=> 'uploads/'.$name,
-        'md5'=> md5_file($path . '/'.$name),
-        'md5_check'=> md5_file($path . '/'.$name) === $md5
-    ]);
-}
+    $total = $_POST['total'];
+    $index = $_POST['index'];
+    $size = $_POST['size'];
 
-if ($file["error"] > 0) {
-    echoJson(400, $file["error"]);
-} else {
-    $res = move_uploaded_file($file['tmp_name'], $dst_file);
-    if($res){
-        file_put_contents($dst_file.'.info', $size);
-        if(mergeFile($path . '/'.$name, $total)){
-            echoJson(200, 'ok', [
-                'url'=> 'uploads/'.$name,
-                'md5'=> md5_file($path . '/'.$name),
-                'md5_check'=> md5_file($path . '/'.$name) === $md5
-            ]);
-        }else{
-            echoJson(201, 'shard ok');
+    $dst_file = $path . '/' . $name . '-' . $total . ':' . $index;
+    if ($file["error"] > 0) {
+        echoJson(400, $file["error"]);
+    } else {
+        $res = move_uploaded_file($file['tmp_name'], $dst_file);
+        if ($res) {
+            file_put_contents($dst_file . '.info', $size);
+            echoJson(200, 'shard ok');
+        } else {
+            echoJson(400, 'shard move_uploaded_file error');
         }
-    }else{
-        echoJson(400, 'shard move_uploaded_file error');
+    }
+} else if ($_GET['type'] == 'merge') {
+    $size = $_POST['size'];
+    $total = $_POST['total'];
+    $msg = '';
+    if (mergeFile($path . '/' . $name, $total, $msg)) {
+        echoJson(200, 'ok', [
+            'url' => 'uploads/' . $name,
+        ]);
+    } else {
+        echoJson(400, $msg);
     }
 }
 
-function mergeFile($name, $total){
-    $already = true;
-    for ($i = 0; $i < $total; $i++){
-        if(!file_exists($name.'-'.$total.':'.$i.'.info') || !file_exists($name.'-'.$total.':'.$i)){;
-            $already = false;
-            break;
-        }else if(filesize($name.'-'.$total.':'.$i) != file_get_contents($name.'-'.$total.':'.$i.'.info')){
-            $already = false;
-            break;
-        }
-    }
-
-    if($already){
-        @unlink($name);
-        if(file_exists($name.'.lock')){
+function mergeFile($name, $total, &$msg)
+{
+    for ($i = 0; $i < $total; $i++) {
+        if (!file_exists($name . '-' . $total . ':' . $i . '.info') || !file_exists($name . '-' . $total . ':' . $i)) {
+            $msg = "shard error $i";
+            return false;
+        } else if (filesize($name . '-' . $total . ':' . $i) != file_get_contents($name . '-' . $total . ':' . $i . '.info')) {
+            $msg = "shard size error $i";
             return false;
         }
-        touch($name.'.lock');
-        $file = fopen($name, 'a+');
-        for ($i = 0; $i < $total; $i++){
-            $shardFile = fopen($name.'-'.$total.':'.$i, 'r');
-            $shardData = fread($shardFile, filesize($name.'-'.$total.':'.$i));
-            fwrite($file, $shardData);
-            fclose($shardFile);
-            unlink($name.'-'.$total.':'.$i);
-            unlink($name.'-'.$total.':'.$i.'.info');
-        }
-        fclose($file);
-        unlink($name.'.lock');
-
-        return true;
     }
-    return false;
+
+    @unlink($name);
+    if (file_exists($name . '.lock')) {
+        $msg = 'on lock';
+        return false;
+    }
+    touch($name . '.lock');
+    $file = fopen($name, 'a+');
+    for ($i = 0; $i < $total; $i++) {
+        $shardFile = fopen($name . '-' . $total . ':' . $i, 'r');
+        $shardData = fread($shardFile, filesize($name . '-' . $total . ':' . $i));
+        fwrite($file, $shardData);
+        fclose($shardFile);
+        unlink($name . '-' . $total . ':' . $i);
+        unlink($name . '-' . $total . ':' . $i . '.info');
+    }
+    fclose($file);
+    unlink($name . '.lock');
+
+    return true;
 }
 
 function echoJson($code, $msg = '', $data = [])
