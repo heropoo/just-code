2bd5e31b5c7ffef39ecdc2914a63ad21cbbac7a6
heropoo <aiyouyou1000@163.com>
2020-04-22 15:46:47 +0800

    parser header

---
+++ php-parse-header/parser.php
@@ -0,0 +1,15 @@
+<?php
+function parse_headers(){
+    $headers = [];
+    foreach ($_SERVER as $name => $value)
+    {
+        if (substr($name, 0, 5) == 'HTTP_')
+        {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+}
+
+$headers = parse_headers();
+var_dump($headers);
