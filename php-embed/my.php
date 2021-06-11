5603ce68b63a4cbe9306e50ce3b948a822f7d7d0
txj <tangxuejia@jarikaya.com>
2021-06-11 13:18:07 +0800

    test

--- php-embed/my.php
+++ php-embed/my.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "hello world！";
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use App\Command\HelloCommand;
+
+$application = new Application();
+
+// ... register commands
+$application->add(new HelloCommand());
+
+$application->run();
